d2ae3ef1c8f49dac5d7da4ec1a6a60011c02be9a
Ethan <itsethanhook@gmail.com>
2024-12-05 12:34:05 -0800

    Update

--- TeamDumpsterFire/Assets/Scripts/Weapons/GunStats.cs
+++ TeamDumpsterFire/Assets/Scripts/Weapons/GunStats.cs
@@ -7,4 +7,15 @@ public class GunStats
     public float fireRateCooldown;
     public int maxAmmoCountPerMag;
     public float reloadCoolDown;
+    public float weaponDura; //number 1-5;
+
+
+
+    public void TakeDamage()
+    {
+        weaponDura -= 1;
+        weaponDura = Mathf.Clamp(weaponDura, 1, 6);
+
+        
+    }
 }
